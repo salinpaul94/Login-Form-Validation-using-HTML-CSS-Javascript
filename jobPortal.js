990749a41a868ab5b1e063f0ba0b4e1115066c9f
salinpaul94 <salinpv@hotmail.com>
2022-10-12 14:02:28 +0530

    next button and back button

--- jobPortal.js
+++ jobPortal.js
@@ -10,28 +10,55 @@ const jobApplication = [
         img: 'images/02 Bookkeeper Resume.jpg',
     },
     {
-        id: 1,
+        id: 3,
         role: 'chef',
         img: 'images/03 Chef Resume.jpg',
     },
     {
-        id: 1,
+        id: 4,
         role: 'contractor',
         img: 'images/04 Contractor Resume.jpg',
     },
     {
-        id: 1,
+        id: 5,
         role: 'nurse',
         img: 'images/05 Nurse Resume.jpg',
     }
 ]
 
 
-
+let imgSrc = 0;
 let roleData = jobApplication.map(value => value.role);
 console.log(roleData);
 
 let img = document.createElement( "img" );
-img.src = "images/01  Fashion Designer Resume.jpg";
-
+img.src = jobApplication[imgSrc].img;
 document.getElementById('resume').appendChild(img);
+
+
+const next = document.getElementById("next");
+next.addEventListener("click", myNext);
+
+function myNext() {
+    imgSrc += 1
+    if(imgSrc < jobApplication.length){
+        img.src = jobApplication[imgSrc].img;
+    } else {
+        imgSrc = 0;
+        img.src = jobApplication[imgSrc].img;
+    }  
+}
+
+const back = document.getElementById("back");
+back.addEventListener("click", myBack);
+
+function myBack() {
+    imgSrc -= 1
+    console.log(imgSrc);
+    if(imgSrc > -1){
+        img.src = jobApplication[imgSrc].img;
+    } else {
+        imgSrc = jobApplication.length-1;
+        img.src = jobApplication[imgSrc].img;
+    }  
+}
