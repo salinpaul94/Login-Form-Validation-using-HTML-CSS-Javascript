ea95dfd889a8bab2de4befac200d346eadcfbd7e
salinpaul94 <salinpv@hotmail.com>
2022-10-13 13:18:46 +0530

    disabling previous and next if last application on each side

--- jobPortal.js
+++ jobPortal.js
@@ -26,14 +26,18 @@ const jobApplication = [
     }
 ]
 
-
+// document.getElementById("previous").innerHTML = "";
 let imgSrc = 0;
+
+
 let roleData = jobApplication.map(value => value.role);
 console.log(roleData);
 
 let img = document.createElement( "img" );
 img.src = jobApplication[imgSrc].img;
 document.getElementById('resume').appendChild(img);
+
+
 
 
 const next = document.getElementById("next");
@@ -43,22 +47,35 @@ function myNext() {
     imgSrc += 1
     if(imgSrc < jobApplication.length){
         img.src = jobApplication[imgSrc].img;
-    } else {
-        imgSrc = 0;
-        img.src = jobApplication[imgSrc].img;
-    }  
+        loadPrevious();
+    }
+    if(imgSrc === jobApplication.length){
+        document.getElementById("next").innerHTML = "";
+    } 
 }
 
-const back = document.getElementById("back");
-back.addEventListener("click", myBack);
+
+if(imgSrc === 0) {
+    document.getElementById("previous").innerHTML = "";
+}
+function loadPrevious() {
+    if(imgSrc > 0){
+        document.getElementById("previous").innerHTML = "<button class='button' id='back'>Previous</button>";
+        const back = document.getElementById("back");
+        back.addEventListener("click", myBack);
+    }
+}
+
+
 
 function myBack() {
     imgSrc -= 1
     console.log(imgSrc);
     if(imgSrc > -1){
         img.src = jobApplication[imgSrc].img;
-    } else {
-        imgSrc = jobApplication.length-1;
-        img.src = jobApplication[imgSrc].img;
-    }  
+        document.getElementById("next").innerHTML = "<button class='button' id='next'>Next</button>";
+        if(imgSrc === 0) {
+            document.getElementById("previous").innerHTML = "";
+        }
+    }
 }
